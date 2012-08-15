cf9e68ebf8027eeda463c792da3c1a7dfc26c80e
Zac Stewart <zgstewart@gmail.com>
2012-08-15 16:30:21 -0400

    Add xlsx  mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,2 +1,3 @@
 Mime::Type.register "application/vnd.ms-excel", :xls
+Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
 Mime::Type.register "application/vnd.ms-excel", :ppl
